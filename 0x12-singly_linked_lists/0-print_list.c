9126c1c92ab4cdd7434e27aaab66356915a2510a
Godwin <dewyn63@gmail.com>
2022-10-20 14:26:00 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,8 +10,9 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t nodes = 0;
+	size_t nelem;
 
+	nelem = 0;
 	while (h)
 	{
 		if (h->str == NULL)
@@ -20,9 +21,9 @@ size_t print_list(const list_t *h)
 		else
 			printf("[%d] %s\n", h->len, h->str);
 
-		nodes++;
+		nelem++;
 		h = h->next;
 	}
 
-	return (nodes);
+	return (nelem);
 }
